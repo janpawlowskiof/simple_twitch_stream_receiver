3d526d89a12aa8dee6acdc041b56f295890e0ad6
juanpablo4l <blondyn526j@gmail.com>
2023-01-02 22:04:16 +0100

    added buildtools

--- simple_twitch_stream_receiver/receiver.py
+++ simple_twitch_stream_receiver/receiver.py
@@ -3,43 +3,23 @@ from typing import Dict, Iterator, Iterable, List
 
 import ffmpeg
 import numpy as np
+import streamlink.plugins.twitch
 from streamlink import Streamlink
+
+from simple_twitch_stream_receiver.exceptions import StreamNotAvailable
 
 
 class SimpleTwitchStreamReceiver(Iterable):
     """
-    An iterable created given URL to a twitch stream and a quality preset.
+    An iterable created given a URL to a twitch stream and a quality preset.
+    Will raise `StreamNotAvailable` if streamer is offline or does not exist.
     """
 
     def __init__(self, url: str, quality: str = "best", timeout: float = 5.0):
         self.quality: str = quality
         self.url: str = url
         self.timeout = timeout
-
-    @property
-    def available_qualities(self) -> List[str]:
-        session = Streamlink()
-        streams = session.streams(self.url)
-        return list(streams.keys())
-
-    @property
-    def _stream_url(self) -> str:
-        session = Streamlink()
-        streams = session.streams(self.url)
-        stream = streams[self.quality]
-        return stream.to_url()
-
-    @property
-    def probe(self) -> Dict:
-        return ffmpeg.probe(self._stream_url, select_streams='v')
-
-    @property
-    def width(self) -> int:
-        return self.probe['streams'][0]['width']
-
-    @property
-    def height(self) -> int:
-        return self.probe['streams'][0]['height']
+        self._session = Streamlink()
 
     def __iter__(self) -> Iterator[np.ndarray]:
         width, height = self.width, self.height
@@ -62,3 +42,27 @@ class SimpleTwitchStreamReceiver(Iterable):
         finally:
             process.stdout.close()
             process.wait()
+
+    @property
+    def _stream_url(self) -> str:
+        streams = self._session.streams(self.url)
+        self._assert_stream_available(streams)
+        stream = streams[self.quality]
+        return stream.to_url()
+
+    def _assert_stream_available(self, streams: Dict[str, streamlink.plugins.twitch.TwitchHLSStream]) -> None:
+        is_stream_available = len(streams) > 0
+        if not is_stream_available:
+            raise StreamNotAvailable(self.url)
+
+    @property
+    def _probe(self) -> Dict:
+        return ffmpeg.probe(self._stream_url, select_streams='v')
+
+    @property
+    def width(self) -> int:
+        return self._probe['streams'][0]['width']
+
+    @property
+    def height(self) -> int:
+        return self._probe['streams'][0]['height']
